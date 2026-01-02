463fa549f6b9d3c45606f9d3c8e98a51bbe0eae7
iRoge <iroge-27@yandex.ru>
2026-01-02 17:52:21 +0300

    Update TronAddress.php

--- src/Models/TronAddress.php
+++ src/Models/TronAddress.php
@@ -14,6 +14,7 @@ class TronAddress extends Model
     protected $fillable = [
         'wallet_id',
         'address',
+        'hex_address',
         'title',
         'watch_only',
         'private_key',
